d20c617ccdd542059639d0ab4bd0d6d2df58d8b0
Diyorbekkdev <diyorbekjuraev77@gmail.com>
2023-11-05 20:26:12 +0500

    saved card;ajf;iasdjf;ajs;lfdjas;lkdfj

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { CurrentSavedPeople, DonutChart } from "./components";
 import { donutData } from "./mocks";
+
 import "./App.scss";
+import SavedInformationCard from "./components/saved-information/saved-information";
+import { savedCard } from "./mocks/saved-card";
 
 function App() {
   return (
@@ -8,6 +11,12 @@ function App() {
       <div className="row gap-1">
         <DonutChart data={donutData} />
         <CurrentSavedPeople />
+      </div>
+      <div className="saved__wrapper">
+         {savedCard?.map((res)=> (
+             <SavedInformationCard title='Saqlash muddati bo’yicha' key={res?.id} {...res} />
+         )) 
+         }
       </div>
     </div>
   );
